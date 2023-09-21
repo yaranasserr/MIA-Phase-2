2964294abc52644d0d7621f687e7216dbf9a26ce
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2023-09-21 14:58:49 +0300

    Update move.ino

--- move.ino
+++ move.ino
@@ -17,6 +17,74 @@ void setup(){
   pinMode(backward_motor2, OUTPUT);
 
 }
+void Stop()
+{
+  digitalWrite(forward_motor1, LOW);
+  digitalWrite(backward_motor1, LOW);
+  digitalWrite(forward_motor2, LOW);
+  digitalWrite(backward_motor2, LOW);
+  Serial.println("stop");
+}
+void forward(int distance) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < distance) {
+    analogWrite(speed_motor1, Speed);
+    digitalWrite(forward_motor1, HIGH);
+    digitalWrite(backward_motor1, LOW);
+    analogWrite(speed_motor2, Speed);
+    digitalWrite(forward_motor2, HIGH);
+    digitalWrite(backward_motor2, LOW);
+    Serial.println("forward");
+  }
+  Stop();
+}
+
+void backward(int distance) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < distance) {
+    analogWrite(speed_motor1, Speed);
+    digitalWrite(forward_motor1, LOW);
+    digitalWrite(backward_motor1, HIGH);
+    analogWrite(speed_motor2, Speed);
+    digitalWrite(forward_motor2, LOW);
+    digitalWrite(backward_motor2, HIGH);
+    Serial.println("backward");
+  }
+  Stop();
+}
+
+void left(int distance) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < distance) {
+    analogWrite(speed_motor1, Speed);
+    digitalWrite(forward_motor1, HIGH);
+    digitalWrite(backward_motor1, LOW);
+    analogWrite(speed_motor2, Speed);
+    digitalWrite(forward_motor2, LOW);
+    digitalWrite(backward_motor2, HIGH);
+    Serial.println("left");
+  }
+  Stop();
+}
+
+void right(int distance) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < distance) {
+    analogWrite(speed_motor1, Speed);
+    digitalWrite(forward_motor1, LOW);
+    digitalWrite(backward_motor1, HIGH);
+    analogWrite(speed_motor2, Speed);
+    digitalWrite(forward_motor2, HIGH);
+    digitalWrite(backward_motor2, LOW);
+    Serial.println("right");
+  }
+  Stop();
+}
+
+void loop(){
+  forward(30)
+
+}
 void loop() {
   forward();
   // put your main code here, to run repeatedly:
